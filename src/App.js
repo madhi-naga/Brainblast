880da98d985b55d3c1e81c0d3d63da5b31a63139
Jasper Leung <jasper.leung123@gmail.com>
2021-03-26 17:58:41 -0400

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import mainMenu from "./Components/mainMenu"
+import Navbar from "./Components/NavBar"
 import Leaderboard from "./Components/Leaderboard"
 import Instructions from "./Components/Instructions"
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -8,6 +9,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <Navbar></Navbar>
       <Route exact path="/" component={mainMenu} />
       <Route path="/home" component={mainMenu} />
       <Route path="/leaderboard" component={Leaderboard} />
---
+++ src/Components/NavBar.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import './NavBar.css';
+
+function Navbar () {
+    return(
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul className="navbar-nav mr-auto">
+            <Link to={"/"}>
+                <div className="navbar-brand">Brainblast</div>
+            </Link>
+            <Link to={"/"}>
+                <li className="nav-item nav-link">Menu</li>
+            </Link>
+            <Link to={"/leaderboard/"}>
+                <li className="nav-item nav-link">Leaderboard </li>
+            </Link>
+            <Link to={"/instructions/"}>
+                <li className="nav-item nav-link">Instructions </li>
+            </Link>
+        </ul>
+    </nav>
+
+    );
+}
+
+export default Navbar;
