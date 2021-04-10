3651e7e747ab7a05571e847a483858aab85dce14
madhi-naga <madhi.naga@gmail.com>
2021-04-10 13:50:50 -0400

    Added styling

--- src/Leaderboard/Leaderboard.jsx
+++ src/Leaderboard/Leaderboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ScoreTable from "./ScoreTable"
+import "./Leaderboard.css";
 
 export default class Leaderboard extends Component {
 
@@ -7,18 +8,22 @@ export default class Leaderboard extends Component {
     render() {
         return (
             <div className="lead-grid">
-                <div className="grid-1">
+                <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet" ></link>
+                <div className="grid">
                     <ScoreTable pathlink="top" id="top" title="Top Overall Scores" />
                     <ScoreTable pathlink="recent" id="rec" title="Recent Scores" />
-                    <ScoreTable pathlink="top/1" id="mini1" title="Top Minigame 1 Scores" />
-                    <ScoreTable pathlink="top/2" id="mini2" title="Top Minigame 2  Scores" />
+                    <ScoreTable pathlink="top/1" id={1} title="Top Minigame 1 Scores" />
                 </div>
-                <div className="grid-2">
-                    <ScoreTable pathlink="top/3" id="mini3" title="Top Minigame 3 Scores" />
-                    <ScoreTable pathlink="top/4" id="mini4" title="Top Minigame 4  Scores" />
-                    <ScoreTable pathlink="top/5" id="mini5" title="Top Minigame 5  Scores" />
+                <div className="grid">
+                    <ScoreTable pathlink="top/2" id={2} title="Top Minigame 2  Scores" />
+                    <ScoreTable pathlink="top/3" id={3} title="Top Minigame 3 Scores" />
+                    <ScoreTable pathlink="top/4" id={4} title="Top Minigame 4  Scores" />
                 </div>
-                
+                <div className="grid">
+                    <ScoreTable pathlink="top/5" id={5} title="Top Minigame 5  Scores" />
+                </div>
+
             </div>
         )
     }
--- src/Leaderboard/ScoreTable.jsx
+++ src/Leaderboard/ScoreTable.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-const urlBackend = 'https://brainblast-be.herokuapp.com'; 
+import "./Leaderboard.css";
+const urlBackend = 'https://brainblast-be.herokuapp.com';
+
 //const urlBackend = 'http://localhost:8080'
 
 export default class ScoreTable extends Component {
@@ -8,26 +10,69 @@ export default class ScoreTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: [ ]
+            list: []
+        }
+    }
+
+    modifyData = (data) => {
+        var parsedata = [];
+        var i;
+        switch(this.props.id) {
+            case 1:
+                parsedata = [];
+                for(i in data) {
+                    parsedata.push({name: data[i].username, score: data[i].minigame_scores.minigame_1});
+                }
+                return parsedata;
+            case 2:
+                parsedata = [];
+                for(i in data) {
+                    parsedata.push({name: data[i].username, score: data[i].minigame_scores.minigame_2});
+                }
+                return parsedata;
+            case 3:
+                parsedata = [];
+                for(i in data) {
+                    parsedata.push({name: data[i].username, score: data[i].minigame_scores.minigame_3});
+                }
+                return parsedata;
+            case 4:
+                parsedata = [];
+                for(i in data) {
+                    parsedata.push({name: data[i].username, score: data[i].minigame_scores.minigame_4});
+                }
+                return parsedata;
+            case 5:
+                parsedata = [];
+                for(i in data) {
+                    parsedata.push({name: data[i].username, score: data[i].minigame_scores.minigame_5});
+                }
+                return parsedata;
+            default: 
+                parsedata = [];
+                for(i in data) {
+                    parsedata.push({name: data[i].username, score: data[i].total_score});
+                }
+                return parsedata;
         }
     }
 
     componentDidMount() {
         axios.get(`${urlBackend}/scores/${this.props.pathlink}`)
-        .then(res => {
-          this.setState({
-              list: res.data
-          });
-          console.log(this.state.list);
-        })
-        .catch(err => console.log(err));
+            .then(res => {
+                this.setState({
+                    list: this.modifyData(res.data)
+                });
+                //console.log(this.state.list);
+            })
+            .catch(err => console.log(err));
     }
 
 
     render() {
         return (
             <div className="table-bordered">
-                <h3>{this.props.title}</h3>
+                <h3 className="lead-header">{this.props.title}</h3>
                 <table>
                     <thead>
                         <tr>
@@ -37,13 +82,15 @@ export default class ScoreTable extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.list.map((item, i) => { return(
-                            <tr>
-                                <td>{i + 1}</td>
-                                <td>{item.username}</td>
-                                <td>{item.total_score}</td>
-                            </tr>
-                        )})}
+                        {this.state.list.map((item, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td>{i + 1}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.score}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             </div>
